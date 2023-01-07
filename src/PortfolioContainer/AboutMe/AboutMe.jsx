ca45cf69dca7c195a78611f58d402f15718959df
“FredGalway” <fmoitry@gmail.com>
2023-01-07 09:52:03 +0700

    About me part beginning

---
+++ src/PortfolioContainer/AboutMe/AboutMe.jsx
@@ -0,0 +1,18 @@
+import React,{useEffect} from 'react'
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
+import ScrollService from '../../utilities/ScrollService'
+import Animations from '../../utilities/Animations'
+// import "./AboutMe.css"
+
+export default function AboutMe(props) {
+  return (
+    <div className='about-me-container screen-container'>
+      <div className='about-me-parent'>
+        <ScreenHeading title={'About Me'} subHeading={'Why Choose Me ?'}/>
+       
+      </div>
+         
+    </div>
+   
+  )
+}
